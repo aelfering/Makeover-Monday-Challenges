1d85a2a873a7890a3f22122662f3f2c0c718f97b
Alex Elfering <anelfering@outlook.com>
2019-12-29 11:56:23 -0600

    Update Visualization Code.R

--- 2019 Week 53 - Most Popular Makeover Monday Topics/Visualization Code.R
+++ 2019 Week 53 - Most Popular Makeover Monday Topics/Visualization Code.R
@@ -11,22 +11,47 @@ mmonday <- read.csv('Downloads by Dataset.csv')
 
 mmonday$date <- dmy(mmonday$date)
 
-mmonday_18_19 <- subset(mmonday, mmonday$Year > 2017) #  Removing 2016 and 2017 data since it appears to be missing several weeks of topics
+#  Removing 2016 and 2017 data since it appears to be missing several weeks of topics
+mmonday_18_19 <- mmonday %>%
+  filter(Year > 2017) %>%
+  as.data.frame()
+
+# How many more downloads in 2019 since 2018?
+mmonday_18_19 %>%
+  filter(Year == 2018, Week <= 52) %>%
+  summarise(Downloads = sum(Downloads))
+
+mmonday_18_19 %>%
+  filter(Year == 2019, Week <= 52) %>%
+  summarise(Downloads = sum(Downloads))
 
 mmonday_top_10 <- mmonday_18_19 %>%
   group_by(Subject) %>%
   summarise(Downloads = sum(Downloads)) %>%
   arrange(desc(Downloads)) %>%
   mutate(Download_Rank = rank(desc(Downloads))) %>%
-  filter(Download_Rank <= 10) %>%
-  select(Subject) %>%
+  select(Subject, Download_Rank) %>%
   ungroup() %>%
   as.data.frame()
 
+mmonday_top_10_flag <- left_join(mmonday_18_19, mmonday_top_10, by = c('Subject' = 'Subject'))
+mmonday_top_10_flag <- mmonday_top_10_flag %>%
+  mutate(Top_10_Flag = ifelse(Download_Rank <= 10, TRUE, FALSE))
 
-ggplot(mmonday_18_19, aes(x = date, y = Downloads)) +
-  geom_step() +
-  geom_point(size = 2, color = ifelse(mmonday_18_19$Subject %in% mmonday_top_10, 'orange', 'black'))
+ggplot(mmonday_top_10_flag, aes(x = date, y = Downloads)) +
+  geom_step(color = 'gray') +
+  geom_point(shape = 21,
+             colour = ifelse(mmonday_top_10_flag$Top_10_Flag == TRUE, "black", "gray"), 
+             fill = "gray", 
+             size = 2, 
+             stroke = 1) +
+  theme_minimal() +
+  labs(title = "#MakeoverMonday Week 53: Do Downloads Correlate with Submissions?",
+       subtitle = "The number of #MakeoverMonday dataset downloads this year sits at 14,657 up from 1,759 downloads last year* through Week 52",
+       caption = "\n*2018 had 52 total weeks of challenges while 2019 will end with 53 total weeks of challenges.\nSource:Data-World\nVisualization by Alex Elfering") +
+  geom_label(aes(x = as.Date('2019-06-15', "%Y-%m-%d"), y = 750, label = "The 10 most popular\ndownloads were all in\nthe latter half of this year."), 
+             hjust = 0, just = 0.5, colour = "#555555", 
+             fill = "white", label.size = NA, family="Helvetica", size = 3)
 
 
 
