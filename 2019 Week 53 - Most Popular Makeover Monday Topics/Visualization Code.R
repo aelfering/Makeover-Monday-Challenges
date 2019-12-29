d3595d9a7b9068d50601de0469a1196bf8ba6478
Alex Elfering <anelfering@outlook.com>
2019-12-29 11:08:17 -0600

    Include top 10 topics

--- 2019 Week 53 - Most Popular Makeover Monday Topics/Visualization Code.R
+++ 2019 Week 53 - Most Popular Makeover Monday Topics/Visualization Code.R
@@ -3,3 +3,32 @@
 
 # By Alex Elfering
 
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+
+mmonday <- read.csv('Downloads by Dataset.csv')
+
+mmonday$date <- dmy(mmonday$date)
+
+mmonday_18_19 <- subset(mmonday, mmonday$Year > 2017) #  Removing 2016 and 2017 data since it appears to be missing several weeks of topics
+
+mmonday_top_10 <- mmonday_18_19 %>%
+  group_by(Subject) %>%
+  summarise(Downloads = sum(Downloads)) %>%
+  arrange(desc(Downloads)) %>%
+  mutate(Download_Rank = rank(desc(Downloads))) %>%
+  filter(Download_Rank <= 10) %>%
+  select(Subject) %>%
+  ungroup() %>%
+  as.data.frame()
+
+
+ggplot(mmonday_18_19, aes(x = date, y = Downloads)) +
+  geom_step() +
+  geom_point(size = 2, color = ifelse(mmonday_18_19$Subject %in% mmonday_top_10, 'orange', 'black'))
+
+
+
+
+
