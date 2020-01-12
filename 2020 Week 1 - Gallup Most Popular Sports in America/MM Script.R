46e0883de61a75961a16374da7ce913318cda8b8
Alex Elfering <anelfering@outlook.com>
2020-01-12 10:28:37 -0600

    Added caption and cleaned up script

--- 2020 Week 1 - Gallup Most Popular Sports in America/MM Script.R
+++ 2020 Week 1 - Gallup Most Popular Sports in America/MM Script.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(ggplot2)
 library(reshape2)
 library(lubridate)
+library(scales)
 
 sports_pop <- read.csv('Sports Popularity.csv')
 
@@ -20,9 +21,6 @@ sports_no_x <- sports_melt %>%
          Percent = Percent/100) %>%
   mutate(Full.Date = paste(Month, Date, Year, sep = "-")) %>%
   mutate(Full.Date = mdy(Full.Date))
-
-min_year <- min(sports_no_x$Year)
-max_year <- max(sports_no_x$Year)
 
 football <- subset(sports_no_x, sports_no_x$Sport == 'Football')
 soccer <- subset(sports_no_x, sports_no_x$Sport == 'Soccer')
@@ -70,16 +68,19 @@ base_graph <- ggplot(sports_no_x, aes(x = Full.Date, y = Percent), group = Sport
              colour="#004D40", 
              size=2) +
   theme_minimal() +
-  scale_y_continuous(labels = function(x) paste0(x*100, "%"))
+  scale_y_continuous(labels = function(x) paste0(x*100, "%")) +
+  scale_x_date(breaks = pretty_breaks(10), limits = c(as.Date("2004-01-01"), as.Date("2018-06-30")))
 
 # Annotations
-base_graph +
+title_graph <- base_graph +
    labs(axis.title.x = 'Year',
         axis.title.y = 'Percent',
         title = "What are American's Favorite Sport to Watch?",
-        subtitle = "")
+        subtitle = "A 2017 Gallup Survey suggests that while a plurality of Americans enjoy watching football, soccer is also on\nthe rise. The percent of respondents who enjoy watching soccer climbed to 7% from 2% in 2004. Football\nmeanwhile has declined to 38% from a peak of 43% in 2007.",
+        caption = "\nVisualization by Alex Elfering\nSource: Gallup\n") +
+  theme(plot.title = element_text(face = 'bold'))
 
-base_graph +
+annotation_graph <- title_graph +
   # Soccer
   geom_label(aes(x = as.Date("2017-05-01"), y = 0.07, label = "Soccer"), 
              hjust = 0, vjust = 0.5, colour = "#555555", 
